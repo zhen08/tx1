7e408b5273adb7ab67cd45db4e40ed9101846dea
Zhen Liu <liuzhen@touchjet.com>
2016-12-26 22:14:10 +1300

    Fix a typo.

--- vscode.sh
+++ vscode.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 cd /work_disk
-get http://node-arm.herokuapp.com/node_latest_armhf.deb
+wget http://node-arm.herokuapp.com/node_latest_armhf.deb
 sudo dpkg -i node_latest_armhf.deb
 sudo apt-get install libx11-dev -y
 sudo rm -rf vscode
