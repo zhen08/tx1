274195f71ded81f27f5157c69b355f0d384d0bef
Zhen <zhen08@gmail.com>
2017-05-26 21:31:18 +1200

    Update inference.sh

--- inference.sh
+++ inference.sh
@@ -3,7 +3,7 @@
 #Build Jetson Inference
 export INFERENCE_ROOT=/home/ubuntu/jetson-inference
 rm -rf $INFERENCE_ROOT
-git clone https://github.com/dusty-nv/jetson-inference.git $INFERENCE_ROOT
+git clone https://github.com/zhen08/jetson-inference.git $INFERENCE_ROOT
 mkdir  $INFERENCE_ROOT/build
 cd $INFERENCE_ROOT/build
 cmake -DCUDA_USE_STATIC_CUDA_RUNTIME=OFF ../
