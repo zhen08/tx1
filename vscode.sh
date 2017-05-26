adecce86f63e4bb271e2ddff7dd9ac8d112a31e5
Zhen <zhen08@gmail.com>
2017-05-26 23:20:44 +1200

    Update vscode.sh

--- vscode.sh
+++ vscode.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 cd /home/ubuntu
-wget http://node-arm.herokuapp.com/node_latest_armhf.deb
-sudo dpkg -i node_latest_armhf.deb
+curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
+sudo apt-get install -y nodejs
 sudo apt-get install libx11-dev -y
 sudo rm -rf vscode
 git clone https://github.com/microsoft/vscode
